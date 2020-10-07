7f8ce63582c9a064eeb3e14c0db82468d1b6ad3a
15623642395 <565253584@qq.com>
2020-10-08 00:36:18 +0800

    zhuhao2

---
+++ src/com/Teacher.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @author ZHUHAO
+ * @date 2020-10-08 0:35
+ */
+public class Teacher {
+    public String getName(){
+        System.out.println("进入Teacher");
+        return "teacher";
+    }
+}
