9e1b2b6c4faedc9ff1d0653b403837f4588d9dff
15623642395 <565253584@qq.com>
2021-02-20 17:17:28 +0800

    同步master

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.Date;
+
 /**
  * @author ZHUHAO
  * @date 2020-10-07 23:38
@@ -7,6 +9,10 @@ package com;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(5);
+		System.out.println(new Date().getTime());
+		System.out.println(8*60*60*1000);
+		System.out.println(new Date().getTime()+8);
+		System.out.println(new Date());
 	}
 }
 
